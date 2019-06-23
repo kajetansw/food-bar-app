a256988a36371980ecdb443123a7ae8efb7e8ddd
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-06-23 12:01:32 +0200

    added recipe form * added elm build and development scripts

--- webapp/src/Main.elm
+++ webapp/src/Main.elm
@@ -1,40 +1,71 @@
-module Main exposing (..)
+module Main exposing (Recipe, initialModel, view)
 
 import Browser
-import Html exposing (Html, div, button, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, form, h1, input, text)
+import Html.Attributes exposing (id, type_)
 
-main = 
-  Browser.sandbox 
-    { init = init
-    , update = update
-    , view = view
+
+type alias Recipe =
+    { title : String
+    , description : String
+    , preparationTime : Int
     }
 
--- MODEL
-type alias Model = Int
 
-init : Model
-init = 
-  0
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
 
--- UPDATE
-type Msg = Increment | Decrement
 
-update : Msg -> Model -> Model
-update msg model = 
-  case msg of 
-    Increment -> 
-      model + 1
-    
-    Decrement ->
-      model - 1
+initialModel : Recipe
+initialModel =
+    { title = ""
+    , description = ""
+    , preparationTime = 0
+    }
 
--- VIEW
-view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+
+view : Recipe -> Html msg
+view recipe =
+    div []
+        [ h1 [] [ text "Add recipe" ]
+        , form []
+            [ div []
+                [ text "Title"
+                , input
+                    [ id "title"
+                    , type_ "text"
+                    ]
+                    []
+                ]
+            , div []
+                [ text "Description"
+                , input
+                    [ id "description"
+                    , type_ "text"
+                    ]
+                    []
+                ]
+            , div []
+                [ text "Preparation time"
+                , input
+                    [ id "prep-time"
+                    , type_ "number"
+                    ]
+                    []
+                ]
+            , div []
+                [ button
+                    [ type_ "submit" ]
+                    [ text "Create recipe" ]
+                ]
+            ]
+        ]
+
+
+update : msg -> Recipe -> Recipe
+update msg model =
+    initialModel
