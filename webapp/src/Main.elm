b97b9cd4828d993a36cd79754bae236c5787659f
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-10-20 11:40:05 +0200

    added function for viewing card with single recipe data

--- webapp/src/Main.elm
+++ webapp/src/Main.elm
@@ -1,9 +1,13 @@
 module Main exposing (Recipe, initialModel, view)
 
 import Browser
-import Html exposing (Html, button, div, form, h1, input, text)
-import Html.Attributes exposing (id, type_)
+import Html exposing (Html, button, div, form, h1, h2, input, p, text)
+import Html.Attributes exposing (class, id, type_)
 import Html.Events exposing (onClick, onInput)
+
+
+
+-- MODEL
 
 
 type alias Recipe =
@@ -13,18 +17,14 @@ type alias Recipe =
     }
 
 
+type alias Model =
+    ( List Recipe, Recipe )
+
+
 type Msg
     = SaveTitle String
     | SaveDescription String
     | SavePreparationTime (Maybe Int)
-
-
-main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
 
 
 initialModel : Recipe
@@ -33,6 +33,10 @@ initialModel =
     , description = ""
     , preparationTime = Just 0
     }
+
+
+
+-- VIEW
 
 
 view : Recipe -> Html Msg
@@ -76,6 +80,32 @@ view recipe =
         ]
 
 
+viewRecipe : Recipe -> Html Msg
+viewRecipe recipe =
+    div [ class "recipe-card" ]
+        [ h2 [] [ text recipe.title ]
+        , p [] [ text recipe.description ]
+        , p [] [ text ("Preparation time: " ++ getPreparationTime recipe) ]
+        ]
+
+
+getPreparationTime : Recipe -> String
+getPreparationTime recipe =
+    let
+        prepTime =
+            Maybe.withDefault 0 recipe.preparationTime
+    in
+    if prepTime >= 0 then
+        String.fromInt prepTime
+
+    else
+        "Unknown"
+
+
+
+-- UPDATE
+
+
 update : Msg -> Recipe -> Recipe
 update msg recipe =
     case msg of
@@ -90,3 +120,15 @@ update msg recipe =
 
         SavePreparationTime Nothing ->
             { recipe | preparationTime = Nothing }
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
